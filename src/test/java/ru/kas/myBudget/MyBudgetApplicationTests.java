3a08ad5bf72f059f0411271d8feab25e65bd3bd7
Anton Komrachkov <a.komrachkov@gmail.com>
2022-10-04 14:25:42 +0600

    Initial commit

---
+++ src/test/java/ru/kas/myBudget/MyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kas.myBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
