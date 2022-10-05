779cd050d240df336d00ad2a5fa158f7d9883c3b
Anton Komrachkov <a.komrachkov@gmail.com>
2022-10-05 17:35:43 +0600

    add telegram EHO bot

---
+++ src/main/java/ru/kas/myBudget/telegramBot/TelegramBot.java
@@ -0,0 +1,47 @@
+package ru.kas.myBudget.telegramBot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String username;
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return this.token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            System.out.println(update);
+
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage(); // Create a message object object
+            message.setChatId(chat_id);
+            message.setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
