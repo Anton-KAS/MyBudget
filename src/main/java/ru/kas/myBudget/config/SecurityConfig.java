b8a09d3f648384a055ddfae995332a584fa4c7fe
Anton Komrachkov <a.komrachkov@gmail.com>
2022-10-05 14:48:22 +0600

    add encryption of password

--- src/main/java/ru/kas/myBudget/config/SecurityConfig.java
+++ src/main/java/ru/kas/myBudget/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.kas.myBudget.services.WebUserDetailsService;
 
@@ -37,11 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(webUserDetailsService);
+        auth.userDetailsService(webUserDetailsService).passwordEncoder(getPasswordEncoder());
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ru/kas/myBudget/services/RegistrationService.java
+++ src/main/java/ru/kas/myBudget/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.kas.myBudget.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kas.myBudget.models.WebUser;
@@ -10,14 +11,17 @@ import ru.kas.myBudget.repositories.WebUserRepository;
 public class RegistrationService {
 
     public final WebUserRepository webUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(WebUserRepository webUserRepository) {
+    public RegistrationService(WebUserRepository webUserRepository, PasswordEncoder passwordEncoder) {
         this.webUserRepository = webUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(WebUser webUser) {
+        webUser.setPassword(passwordEncoder.encode(webUser.getPassword()));
         webUserRepository.save(webUser);
     }
 }
