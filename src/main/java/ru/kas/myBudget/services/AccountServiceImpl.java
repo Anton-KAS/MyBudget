fbdd1ddd46a84f9a7f42ff9a0e09a30ac46f1b6f
Anton Komrachkov <a.komrachkov@gmail.com>
2022-11-02 16:11:12 +0600

    Add text to account page menu t9

--- src/main/java/ru/kas/myBudget/services/AccountServiceImpl.java
+++ src/main/java/ru/kas/myBudget/services/AccountServiceImpl.java
@@ -4,21 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kas.myBudget.models.Account;
+import ru.kas.myBudget.repositories.AccountRepository;
 
 @Service
 @Transactional(readOnly = true)
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService {
 
-    private final AccountService accountService;
+    private final AccountRepository accountRepository;
 
     @Autowired
-    public AccountServiceImpl(AccountService accountService) {
-        this.accountService = accountService;
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
     }
 
     @Override
     @Transactional
     public void save(Account account) {
-        accountService.save(account);
+        accountRepository.save(account);
     }
 }
