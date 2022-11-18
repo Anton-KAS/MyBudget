9ba83dc0349e89cb37f1ab479a05c1c77e2f14b6
Anton Komrachkov <a.komrachkov@gmail.com>
2022-11-18 22:15:38 +0600

    add AddAccountNames order test

--- src/test/java/ru/kas/myBudget/bots/telegram/dialogs/addAccount/AddAccountNamesTest.java
+++ src/test/java/ru/kas/myBudget/bots/telegram/dialogs/addAccount/AddAccountNamesTest.java
@@ -1,2 +1,30 @@
-package ru.kas.myBudget.bots.telegram.dialogs.addAccount;public class AddAccountNamesTest {
+package ru.kas.myBudget.bots.telegram.dialogs.addAccount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static ru.kas.myBudget.bots.telegram.dialogs.addAccount.AddAccountNames.*;
+
+public class AddAccountNamesTest {
+    @Test
+    public void orderTest() {
+        //given
+        AddAccountNames[] expectedOrder = {
+                START,
+                TYPE,
+                TITLE,
+                DESCRIPTION,
+                CURRENCY,
+                BANK,
+                START_BALANCE,
+                CONFIRM,
+                SAVE
+        };
+
+        //then
+        AddAccountNames[] resultOrder = AddAccountNames.values();
+
+        //when
+        assertArrayEquals(expectedOrder, resultOrder);
+    }
 }
